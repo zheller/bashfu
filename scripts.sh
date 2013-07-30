c9afdc3ddb7ba462d18e004c420132863e06fa2d
Zachary Wright Heller <zheller@gmail.com>
2013-07-29 23:07:52 -0700

    add thing to find things running on ports and stuff

--- scripts.sh
+++ scripts.sh
@@ -3,3 +3,7 @@
 # Find really big files that are clogging up the hard drive
 # and print them out as a list with human readable sizes
 find / 2>/dev/null -size +100000k -print0 | xargs -0 ls -lh
+
+# Find processes that are listening to ports on my OSX
+# So I can kill them with a vengence cuz they are assholes
+lsof -i | grep LISTEN
